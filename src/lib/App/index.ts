360a0fa852318526077f11e6a18979042aa4dfb9
NJ <nj.namju@gmail.com>
2022-11-09 10:40:20 -0500

    update package

--- src/lib/App/index.ts
+++ src/lib/App/index.ts
@@ -1,7 +1,11 @@
 // import { CanvasCore } from './Implementation/CanvasCore';
 import { ThreeCore } from './Implementation/ThreeCore';
+
+import { version } from 'njscore';
+
 export class App {
     constructor() {
+        console.log(version);
         // new CanvasCore('SandBox');
         new ThreeCore('SandBox');
         console.log('Sandbox starter!!!!!');
